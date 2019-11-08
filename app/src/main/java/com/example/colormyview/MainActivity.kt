e66dfc8e4a5d8bffcdde3ca73ce640af7a45a47f
taruc <chongxe-wa16@student.tarc.edu.my>
2019-11-08 14:46:14 +0800

    second update

--- app/src/main/java/com/example/colormyview/MainActivity.kt
+++ app/src/main/java/com/example/colormyview/MainActivity.kt
@@ -30,15 +30,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeColored(view: View) {
-        when (view.id) {  //switch(id)in java or c
+        when (view.id) {
 
             // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(R.drawable.rosegoldimage)
-            R.id.box_two_text -> view.setBackgroundColor(R.drawable.image1)
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            else -> view.setBackgroundColor(R.drawable.rosegoldimage)
         }
     }
 }
